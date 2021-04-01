4208970b9e0e19f37ebaef22009ecd6669ba4a1d
Angat Yogi <angat.yogi@ou.edu>
2021-04-01 16:53:54 -0500

    All Problems Added.

--- ExerciseStringBuilder/ExerciseStringBuilder/Program.cs
+++ ExerciseStringBuilder/ExerciseStringBuilder/Program.cs
@@ -7,7 +7,7 @@ namespace ExerciseStringBuilder
     {
         static void Main(string[] args)
         {
-            Problem3();
+            Problem5ii();
         }
         static void Problem1()
         {   
@@ -96,20 +96,103 @@ namespace ExerciseStringBuilder
             }
         }
 
-        static void Problem3()
+        public static void Problem3()
         {
-            Console.WriteLine("Name Please");
-            string name = Console.ReadLine();
+            Console.WriteLine("Enter a time value in 24-hour time format. Example, 19:00.");
+            string problem3Input = Console.ReadLine();
 
-            char[] array = new char[name.Length];
-
-            for (int i = name.Length; i >0; i--)
+            if (String.IsNullOrWhiteSpace(problem3Input))
+                Console.WriteLine("Invalid Time");
+            
+            var array = problem3Input.Split(':');
+            if (array[1].Length!=2)
             {
-                array[name.Length - i] = name[i-1];
-                
+                Console.WriteLine("Invalid Time");
+                return;
             }
-            String reversed =new string(array);
-            Console.WriteLine(reversed);
+
+            try
+            {
+
+            if(Convert.ToInt32(array[0])<0 || Convert.ToInt32(array[0])>23 || Convert.ToInt32(array[1]) > 59|| Convert.ToInt32(array[1])<0)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+            else
+            {
+                Console.WriteLine("Ok");
+            }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Time");
+            }
         }
+        public static void Problem4()
+        {
+            Console.WriteLine("Enter words separated by space (Example: Hello everyone I hope all is well).");
+            string problem4Input = Console.ReadLine();
+
+            string[] inputs = problem4Input.Split(' ');
+
+            string addedString = "";
+            foreach(string userInput in inputs)
+            {
+                string lowerCaseInput = userInput.ToLower();
+                string result=lowerCaseInput[0].ToString().ToUpper();
+                for (int i = 1; i < lowerCaseInput.Length; i++)
+                {
+                    result += lowerCaseInput[i];
+                }
+
+                addedString += result;
+            }
+                Console.WriteLine(addedString);
+        }
+        public static void Problem4ii()
+        {
+            Console.WriteLine("Enter words separated by space (Example: Hello everyone I hope all is well).");
+            string problem4Input = Console.ReadLine();
+
+            string addedString = "";
+            foreach(string userInput in problem4Input.Split(' '))
+            {               
+                string result=char.ToUpper(userInput[0])+ userInput.ToLower().Substring(1);
+                addedString += result;
+            }
+                Console.WriteLine(addedString);
+        }
+        public static void Problem5()
+        {
+            Console.WriteLine("Enter an English word.");
+            string input = Console.ReadLine();
+
+            int count = 0;
+            foreach (char character in input.ToLower())
+            {
+                if (character=='a'|| character == 'e'|| character == 'i'|| character == 'o'|| character == 'u')
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine("No of vowels in the input is: {0}",count);
+        }
+        public static void Problem5ii()
+        {
+            Console.WriteLine("Enter an English word.");
+            string input = Console.ReadLine();
+            List<char> characters = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            int count = 0;
+            foreach (char character in input.ToLower())
+            {
+                if (characters.Contains(character))
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine("No of vowels in the input is: {0}",count);
+        }
+
+
     }
 }

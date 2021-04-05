bed11d12fa70dee923e0d69f2441d5551744b9a0
Angat Yogi <angat.yogi@ou.edu>
2021-04-05 14:49:08 -0500

    Files.ReadAllLines practised

---
+++ Files_ReadingInCSVAndParsing/Files_ReadingInCSVAndParsing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Files_ReadingInCSVAndParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("geoMap.csv");
+            List<string> states = new List<string>();
+            List<double> soccerScores = new List<double>();
+            List<double> footballScores = new List<double>();
+
+            for (int i=3;i< lines.Length;i++)
+            {
+                //Regional/state, SOCCER, FOOTBALL
+                //Alabama,16%,84%
+                string[] lineItem = lines[i].Split(",");
+                
+                    string state = lineItem[0];
+                    string soccerScore = lineItem[1].Trim('%');
+                    
+                    string footballScore = lineItem[2].Trim('%');
+                    
+                    states.Add(state);
+                    soccerScores.Add(Convert.ToDouble(soccerScore)/100);
+                    footballScores.Add(Convert.ToDouble(footballScore)/100);
+                
+
+            }
+
+            for (int i = 0; i < footballScores.Count; i++)
+            {
+                string state = states[i];
+                if (footballScores[i]<soccerScores[i])
+                {
+                    Console.WriteLine(state);
+                }
+            }
+        }
+    }
+}

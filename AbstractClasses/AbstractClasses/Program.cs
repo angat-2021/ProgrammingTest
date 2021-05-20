ce8499228d7aceb67fb7613c1ad978a0f0975baa
Angat Yogi <angat.yogi@ou.edu>
2021-05-20 16:56:15 -0500

    Abstract Classes completed

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractClasses
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var shape = new Shape();
+           //Cannot create an object of an abstarct class
+
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+
+            //sealed is opposite of a abstract. It prevents the derivation of a class or overriding its members.
+
+            
+        }
+    }
+}
---
+++ AbstractClasses/AbstractClasses/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+
+        public void Copy()
+        {
+            Console.WriteLine("The shape is copied.");
+        }
+        public void Select()
+        {
+            Console.WriteLine("Select the shape.");
+        }
+    }
+}
